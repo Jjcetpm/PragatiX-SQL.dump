08b9dbc37f434b664aa6d84a435917550e2057f9
JJCET - PM <jjcetpm@jjcet.ac.in>
2026-08-27 10:01:19 +0530

    Rename database from spdms_lab to pragatix

--- Dump20260826.sql
+++ Dump20260826.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `spdms_lab` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `spdms_lab`;
+CREATE DATABASE  IF NOT EXISTS `pragatix` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `pragatix`;
 -- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: spdms_lab
